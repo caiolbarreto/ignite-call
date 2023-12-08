fbc390f0c70c64ccdf9a60ea5f5415fc93feaeb8
Caio Barreto <caiobarreto_28@hotmail.com>
2023-12-08 20:02:19 -0300

    feat: schedulings table and date format in calendar component

--- src/app/schedule/[username]/schedule-form/calendar-step/index.tsx
+++ src/app/schedule/[username]/schedule-form/calendar-step/index.tsx
@@ -2,6 +2,7 @@ import { Calendar } from '@/components/Calendar'
 import { Container, TimePickerHeader } from './styles'
 import { twMerge } from 'tailwind-merge'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 const buttonStyles = twMerge(
   'border-none bg-gray600 py-3 cursor-pointer text-gray100 rounded-md text-sm leading-3',
@@ -12,13 +13,16 @@ export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
   const isTimePickerOpen = !!selectedDate
 
+  const weekDay = selectedDate && dayjs(selectedDate).format('dddd')
+  const dayAndMonth = selectedDate && dayjs(selectedDate).format('MMMM[ ]DD')
+
   return (
     <Container isTimePickerOpen={isTimePickerOpen}>
       <Calendar selectedDate={selectedDate} onSelectDate={setSelectedDate} />
       {isTimePickerOpen && (
         <div className="border-l border-l-gray600 px-6 pt-6 overflow-y-scroll absolute top-0 bottom-0 right-0 w-[280px]">
           <TimePickerHeader>
-            Tuesday <span>december 20</span>
+            {weekDay} <span>{dayAndMonth}</span>
           </TimePickerHeader>
 
           <div className="mt-3 grid grid-cols-2 gap-2 full lg:grid-cols-1 ">
